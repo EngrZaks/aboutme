6fb493523749db7648021c6b3826c450e3066b1b
EngrZaks <abzakariyya@gmail.com.com>
2021-03-07 10:51:09 +0100

    added link to my resume on about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -17,7 +17,7 @@ export default function About() {
                padding: "10px",
             }}>
             <p>
-               I am an evolving self-taught full stack web developer who is
+               I am an evolving, self-taught full stack web developer, who is
                passionate about building scalable and fast cloud solutions. I
                would love to form part of a formidable tech team that can help
                solve some of African problems creatively and innovatively.
@@ -25,13 +25,13 @@ export default function About() {
             <p>
                I am currently working on a web portal that shows physical
                properties of various agricultural products based on a database
-               of researches that have been conducted previously and in the
-               future as my undergraduate project at Ahmadu Bello University,
-               Zaria Nigeria. I am also working on UI and performance
-               improvement for Sindhi Dry Fruits (New Delhi, India.)
+               of researches that are been conducted from other studies as my
+               undergraduate project at Ahmadu Bello University, Zaria Nigeria.
+               I am also working on UI and performance improvement for Sindhi
+               Dry Fruits (New Delhi, India.)
             </p>
             <p>
-               I am open to opportunities, Jobs, collaboration or freelancing
+               I am open to opportunities, Jobs, collaboration or freelancing.
             </p>
             <p>
                Appart from coding and learning, I love to travel-even though
@@ -40,6 +40,16 @@ export default function About() {
                interesting documentaries, with my favorite being Air Crash
                Investigation ✈.
             </p>
+            <a
+               style={{
+                  padding: "15px 5px",
+                  color: "lime",
+                  margin: "20px 0",
+                  fontWeight: "bold",
+               }}
+               href='https://drive.google.com/file/d/136nhlywrYqm35i3btbFJ6BVm2pklZznY/view?usp=sharing'>
+               My Resume
+            </a>
          </div>
       </div>
    );
